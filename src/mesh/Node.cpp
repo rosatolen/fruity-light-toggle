2f67f11c99d6fdfcc3188e46d2a4d97f8a0d3743
tomclement <tc.oketch@gmail.com>
2015-10-26 15:58:25 -0700

    Tom <3 Pete - Make gateway node blink purple

--- src/mesh/Node.cpp
+++ src/mesh/Node.cpp
@@ -946,31 +946,50 @@ void Node::TimerTickHandler(u16 timerMs)
 
 	//FIXME: there should be a handshake timeout
 
+	//trace("Tick, currentLedMode: %d\r\n",currentLedMode);
 	if (currentLedMode == LED_MODE_CONNECTIONS)
 	{
 		//Now we test for blinking lights
 		u8 countHandshake = (cm->inConnection->handshakeDone ? 1 : 0) + (cm->outConnections[0]->handshakeDone ? 1 : 0) + (cm->outConnections[1]->handshakeDone ? 1 : 0) + (cm->outConnections[2]->handshakeDone ? 1 : 0);
 		u8 countConnected = (cm->inConnection->isConnected ? 1 : 0) + (cm->outConnections[0]->isConnected ? 1 : 0) + (cm->outConnections[1]->isConnected ? 1 : 0) + (cm->outConnections[2]->isConnected ? 1 : 0);
+		bool noConnections = countHandshake == 0 && countConnected == 0;
 
-		u8 i = ledBlinkPosition / 2;
+		u8 i = ledBlinkPosition/ 2;
+
+
+		//trace("making lights. ledBlinkPosition is %d. connCount is %d. handshakeCount is %d \r\n",ledBlinkPosition,countConnected,countHandshake);
 
 		if(i < Config->meshMaxConnections){
-			if(ledBlinkPosition % 2 == 0){
-				//Connected and handshake done
-				if(cm->connections[i]->handshakeDone) { LedBlue->On(); }
-				//Connected and handshake done
-				if(!cm->connections[i]->handshakeDone && cm->connections[i]->isConnected) { LedGreen->On(); }
-				//A free connection
-				if(!cm->connections[i]->handshakeDone && !cm->connections[i]->isConnected) {  }
-				//No connections
-				if(countHandshake == 0 && countConnected == 0) { LedRed->On(); }
-			} else {
+			bool handshakeDone = cm->connections[i]->handshakeDone;
+			bool handshakeInProgress = !cm->connections[i]->handshakeDone && cm->connections[i]->isConnected;
+
+			if(ledBlinkPosition % 2 == 0) {
+
+				if (handshakeDone) { LedBlue->On(); }
+
+				if (handshakeInProgress) { LedGreen->On(); }
+
+				if (noConnections) { LedRed->On(); }
+
+			}else{
 				LedRed->Off();
 				LedGreen->Off();
 				LedBlue->Off();
 			}
+		}else{
+			if( isGatewayDevice ){
+					// purple
+					LedRed->On();
+					LedGreen->Off();
+					LedBlue->On();
+				}
+				else{
+					// off
+					LedRed->Off();
+					LedGreen->Off();
+					LedBlue->Off();
+				}
 		}
-
 		ledBlinkPosition = (ledBlinkPosition + 1) % ((Config->meshMaxConnections + 2) * 2);
 
 /*
