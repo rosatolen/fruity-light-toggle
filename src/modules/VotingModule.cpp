86835a205a873b9c71d1cf82d3e9da632d95ff15
Rosalie Tolentino <rtolenti@thoughtworks.com>
2015-10-28 13:38:07 -0700

    Rosalie <3 Ashley: Saves Working Retry Queue

--- src/modules/VotingModule.cpp
+++ src/modules/VotingModule.cpp
@@ -16,6 +16,74 @@ extern "C"{
 #define BUTTON_DEBOUNCE_DELAY   50
 #define APP_GPIOTE_MAX_USERS    1
 
+#define MAX_QUEUE_SIZE 5
+
+bool INITIALIZED_QUEUE = false;
+
+typedef struct Queue {
+	short capacity;
+	short size;
+	short front;
+	short rear;
+	short *elements;
+} Queue;
+
+static Queue * failed = NULL;
+
+void createQueue(int maxElements) {
+	failed = (Queue *)malloc(sizeof(Queue));
+	failed->elements = (short *)malloc(sizeof(short)*maxElements);
+	failed->size = 0;
+	failed->capacity = maxElements;
+	failed->front = 0;
+	failed->rear = -1;
+}
+
+short dequeue(Queue *Q) {
+	// cannot dequeue if empty
+	if (Q->size==0) {
+		return -1;
+	} else {
+		Q->size--;
+		Q->front++;
+		if(Q->front == Q->capacity) {
+			Q->front=0;
+		}
+	}
+	return 0;
+}
+
+short front(Queue *Q) {
+	// queue is empty
+	if (Q->size==0) {
+		return -1;
+	}
+	return Q->elements[Q->front];
+}
+
+short enqueue(Queue *Q, short element) {
+	// queue is full
+	if (Q->size == Q->capacity) {
+		return -1;
+	} else {
+		Q->size++;
+		Q->rear = Q->rear + 1;
+		if (Q->rear == Q->capacity) {
+			Q->rear = 0;
+		}
+		Q->elements[Q->rear] = element;
+	}
+	return 0;
+}
+
+void setupQueue(int maxElements) {
+	createQueue(MAX_QUEUE_SIZE);
+	enqueue(failed, 3566);
+	enqueue(failed, 1234);
+	enqueue(failed, 5432);
+	enqueue(failed, 9999);
+}
+
 static void vote(unsigned short uID) {
 	ConnectionManager *cm = ConnectionManager::getInstance();
 	Logger::getInstance().enableTag("VOTING");
@@ -34,6 +102,7 @@ static void vote(unsigned short uID) {
 	packet.data[1] = (uID >> 8) & 0xff;
 	cm->SendMessageToReceiver(NULL, (u8*)&packet, SIZEOF_CONN_PACKET_MODULE + 3 + 1, true);
 	logt("VOTING", "Sending vote with id: %d\n", uID);
+	// TODO store id in failed queue until ack received
 }
 
 	VotingModule::VotingModule(u16 moduleId, Node* node, ConnectionManager* cm, const char* name, u16 storageSlot)
@@ -71,21 +140,27 @@ void VotingModule::ConfigurationLoadedHandler()
 
 void VotingModule::TimerEventHandler(u16 passedTime, u32 appTimer)
 {
+	if (!INITIALIZED_QUEUE) {
+		 setupQueue(MAX_QUEUE_SIZE);
+		 INITIALIZED_QUEUE=true;
+		logt("VOTING", "Initialized da failures\n");
+	}
+
 	if (!node->isGatewayDevice) {
 		// if 10 seconds have passed
 		if ((appTimer / 1000) % 10 == 0 && (appTimer / 100) % 100 == 0) {
-			unsigned short failed[] = { 3566, 1234, 4321 };
-			// using sizeof(failed) is buggy
-			for (int i=0; i < 3; i++){
-				vote(failed[i]);
+			//TODO if i should retry...
+			for (int i=0; i < failed->size; i++){
+				vote(failed->elements[i]);
 			}
 		}
+		// EVERY SECOND
 		if ((appTimer / 1000) % 5 == 0) {
 			//Send a Heartbeat
 			connPacketModule packet;
 			packet.header.messageType = MESSAGE_TYPE_MODULE_TRIGGER_ACTION;
 			packet.header.sender = node->persistentConfig.nodeId;
-			packet.header.receiver = 0;
+			packet.header.receiver = 0; // send to everyone
 			packet.moduleId = moduleID::VOTING_MODULE_ID;
 			packet.actionType = 0; // hardcoded from the reference VotingModule.h
 			packet.data[0] = 5;
@@ -143,7 +218,6 @@ void VotingModule::ConnectionPacketReceivedEventHandler(connectionPacket* inPack
 	// Voter Receives acknowledgement from Gateway
 	if(packetHeader->messageType == MESSAGE_TYPE_MODULE_ACTION_RESPONSE){
 		connPacketModule* packet = (connPacketModule*)packetHeader;
-		// if it is meant for me...
 		if(packet->moduleId == moduleId)
 		{
 			if(packet->actionType ==VotingModuleActionResponseMessages::RESPONSE_MESSAGE)
@@ -152,8 +226,8 @@ void VotingModule::ConnectionPacketReceivedEventHandler(connectionPacket* inPack
 				if(packet->data[0] != 5) {
 					unsigned short userId = (( (short)packet->data[1] ) << 8) | packet->data[0];
 					logt("VOTING", "Voter received acknowledgement from Gateway with userId: %d \n", userId);
+					// TODO: Remove ID from Queue
 				}
-				//TODO make voting stop
 			}
 		}
 	}
