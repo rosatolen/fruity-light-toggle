76bde7161f3f6d81bd03093be4aeac6497dd49fa
Pete Hodgson <git@thepete.net>
2015-10-29 10:42:14 -0700

    final tweaks to heartbeat module

--- inc/HeartbeatModule.h
+++ inc/HeartbeatModule.h
@@ -9,4 +9,7 @@ class HeartbeatModule : public Module
 
 		void TimerEventHandler(u16 passedTime, u32 appTimer);
 		void ConnectionPacketReceivedEventHandler(connectionPacket* inPacket, Connection* connection, connPacketHeader* packetHeader, u16 dataLength);
+
+  private:
+		ModuleConfiguration _configuration;
 };
--- src/modules/HeartbeatModule.cpp
+++ src/modules/HeartbeatModule.cpp
@@ -20,6 +20,11 @@ HeartbeatModule::HeartbeatModule(Node* node, ConnectionManager* cm, const char* 
 	: Module(moduleID::HEARTBEAT_MODULE_ID, node, cm, name, storageSlot)
 {
 	Logger::getInstance().enableTag("HEARTBEAT");
+
+  _configuration.moduleId = moduleID::HEARTBEAT_MODULE_ID;
+  _configuration.moduleVersion = 1;
+  _configuration.moduleActive = true;
+  configurationPointer = &_configuration;
 }
 
 void fillConnectionStruct(Connection *conn, connection &connStruct){
@@ -32,7 +37,8 @@ void fillConnectionStruct(Connection *conn, connection &connStruct){
 
 void HeartbeatModule::TimerEventHandler(u16 passedTime, u32 appTimer)
 {
-		if ((appTimer / 1000) % 5 == 0) {
+    // fire every 5 seconds. TODO: make this not confusing and flakey
+		if ((appTimer / 1000) % 5 == 0 && appTimer % 10 == 0) {
 			connPacketHeartbeat packet;
 
 			packet.header.messageType = MESSAGE_TYPE_HEARTBEAT;
@@ -57,6 +63,10 @@ void HeartbeatModule::ConnectionPacketReceivedEventHandler(connectionPacket* inP
 
   connPacketHeartbeat* packet = (connPacketHeartbeat*)packetHeader;
 
-  logt("HEARTBEAT", "HEARTBEAT RECEIVED from nodeId:%d\n", packetHeader->sender);
-  // TODO: log node's connections
+  logt("HEARTBEAT", "HEARTBEAT RECEIVED from nodeId:%d inConn:%d outConns:[%d,%d,%d]\n", 
+      packetHeader->sender,
+      packet->inConn.partnerId,
+      packet->outConn[0].partnerId,
+      packet->outConn[1].partnerId,
+      packet->outConn[2].partnerId);
 }
